e9063772fb622d7765ef965ca9dbb39c7c9e283e
Dmitry <dmitry@gmail.com>
2010-10-14 20:00:37 +0400

    DSNs page design minor improvements

--- tpl/dsns.php
+++ tpl/dsns.php
@@ -1,24 +1,24 @@
 <form action="dsns.php" method="post">
 <table width="100%">
 <thead>
-	<tr>
+	<tr bgcolor="#EEEEEE">
 		<td width="1">ID</td>
 		<td width="1">Name</td>
-		<td width="1">DSN to connect</td>
+		<td width="1">DSN to connect <a href="http://ru.php.net/manual/en/pdo.drivers.php">[?]</a></td>
 		<td width="1">Delete?</td>
 	</tr>
 </thead>
 <tbody>
 	<?foreach ($_POST['dsns'] as $id => $dsn) if ($id) {?>
 		<input type="hidden" name="dsns[<?=$dsn['id']?>][id]" value="<?=$dsn['id']?>" />
-		<tr>
+		<tr bgcolor="#F5F5F5">
 		<td width="1%"><?=$dsn['id']?></td>
 		<td width="1%"><input type="text" name="dsns[<?=$dsn['id']?>][name]" size="20" /></td>
 		<td width="97%"><input type="text" name="dsns[<?=$dsn['id']?>][value]" size="90" style="width:100%"/></td>
 		<td width="1%"><input type="checkbox" name="dsns[<?=$dsn['id']?>][delete]" value="1"/></td>
 		</tr>
 	<?}?>
-	<tr>
+	<tr bgcolor="#F5F5F5">
 	<td>NEW</td>
 	<td><input type="text" name="dsns[0][name]" size="20"/></td>
 	<td><input type="text" name="dsns[0][value]" size="90" style="width:100%"/></td>
